76d43a71220ede5f17c049629cbae7d0e1a5565e
wilace <170810759+wilace@users.noreply.github.com>
2024-06-17 23:56:50 +0900

    added some nvim's options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,18 +1,22 @@
 local options = {
  -- { name, value, opts }
 
+    { 'termguicolors', true, {} },
+    { 'mouse', '', {} },
+
     { 'tabstop', 4, {} },
     { 'shiftwidth', 0, {} },
     { 'expandtab', true, {} },
 
-    { 'number', true, {} },
+    { 'number', true, { scope = 'local', win = 0 } },
+    { 'scrolloff', 10, { scope = 'local', win = 0 } },
 }
 
 
-local length = #options
+local array_length = #options
 local nvim_set_option_value = vim.api.nvim_set_option_value
 
-for i=1, length do
+for i=1, array_length do
     local option = options[i]
     nvim_set_option_value(
         option[1],  -- name
@@ -20,3 +24,9 @@ for i=1, length do
         option[3]   -- opts
     )
 end
+
+--[[
+    list of items to be set.
+
+]]
+
