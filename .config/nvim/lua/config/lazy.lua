1998a63acff013ae3d72eb04bffe8d4710c4fafd
wilace <170810759+wilace@users.noreply.github.com>
2024-06-21 09:15:16 +0900

    added easy configuration of lazy.nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,6 +1,40 @@
 local lazy = {}
 
 lazy.load_lazy = function()
+    local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+
+    if not (vim.uv or vim.loop).fs_stat(lazypath) then
+        vim.fn.system({
+            'git',
+            'clone',
+            '--filter=blob:none',
+            'https://github.com/folke/lazy.nvim.git',
+            '--branch=stable', -- latest stable release
+            lazypath,
+        })
+    end
+    vim.opt.runtimepath:prepend(lazypath)
+
+    require('lazy').setup('plugins', {
+        defaults = {
+            lazy = true,
+            version = '*',
+        },
+        performance = {
+            rtp = {
+                disabled_plugins = {
+                    "gzip",
+                    "matchit",
+                    "matchparen",
+                    "netrwPlugin",
+                    "tarPlugin",
+                    "tohtml",
+                    "tutor",
+                    "zipPlugin",
+                },
+            },
+        },
+    })
 end
 
 return lazy
