538731b709023acbb1c59526725617873a0ce56b
wilace <170810759+wilace@users.noreply.github.com>
2025-12-13 17:56:21 +0900

    vscode: [Add] build config

---
+++ .config/vscode/copy_build_config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+project_dir=${1}
+
+if [[ ! -d "${project_dir}" ]]; then
+  mkdir -v ${project_dir}
+fi
+
+if [[ ! -d "${project_dir}/.vscode" ]]; then
+  mkdir -v "${project_dir}/.vscode"
+fi
+
+cp -v "$(dirname "${0}")/tasks.json" "$(dirname "${0}")/launch.json" "${project_dir}/.vscode/"
+
+if [[ -f "../clang/copy_clang_config.sh" ]]; then
+  ../clang/copy_clang_config.sh "${project_dir}"
+fi
+
+unset project_dir
